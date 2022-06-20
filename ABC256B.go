f8144ee1b4a5a0db7c277eb639f3d8fa2285888f
n_na <91656188+n-nana@users.noreply.github.com>
2022-06-20 14:58:24 +0900

    Create ABC256B.go

---
+++ ABC256B.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+import "math"
+
+func main(){
+    var N,a int
+    P := make([]int, 5)
+    fmt.Scan(&N)
+
+    for i := 0; i<N; i++{
+        fmt.Scan(&a)
+        P[0]++
+        for j := 0; j<4; j++{
+            var crr = 3-j
+            var val = float64(crr+a)
+            var nxt = int(math.Min(val,4))
+            P[nxt] += P[crr]
+            P[crr] = 0
+        }
+    }
+    fmt.Println(P[4])
+}
+
